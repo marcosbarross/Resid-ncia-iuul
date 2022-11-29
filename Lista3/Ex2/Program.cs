f851ef79db3a8271bbfafc63634bc08931feb1b6
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2022-11-29 11:25:39 -0300

    Adiciona exercicios 2 e 3 da lista 3

---
+++ Lista3/Ex2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static Lista3.Program;
+
+namespace Lista3
+{
+    internal class Program
+    {
+        public class IndiceRemissivo
+        {
+            List<string> list = new List<string>();
+            List<string> imprimir = new List<string>();
+
+            
+            public void indice(string pathTXT)
+            {
+                var contents = File.ReadAllLines(pathTXT);
+                
+                for (int i = 0; i < contents.Length; i++)
+                {
+                    string linha = contents[i].ToString();
+                    string[] palavras = linha.Split(' ');
+
+                    foreach (var palavra in palavras)
+                    {
+                        list.Add(palavra);
+                    }
+
+                    foreach (var palavra in palavras)
+                    {
+
+                        if(palavra.Length > 1 && imprimir.Contains(palavra) == false)
+                        {
+                            imprimir.Add($"{palavra.ToUpper()}, {list.IndexOf(palavra)}, {i}");
+                        }
+                    }
+
+                }
+
+
+
+                imprimir.Sort();
+            }
+
+            public void Imprime(string outFile)
+            {
+                File.WriteAllLines(outFile, imprimir);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            IndiceRemissivo i = new IndiceRemissivo();
+            i.indice(@"C:\Users\marco\Downloads\Unidade 1 - Exercicios - Parte 3\texto.txt");
+            i.Imprime(@"C:\Users\marco\Downloads\Unidade 1 - Exercicios - Parte 3\someOutFile.txt");
+        }
+
+    }
+        
+}
---
+++ Lista3/ex3NumerosDeArmstrong/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Net.Security;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using static ex3NumerosDeArmstrong.Program;
+
+namespace ex3NumerosDeArmstrong
+{
+    internal class Program
+    {
+        public class na
+        {
+
+            public bool IsArmstrong(int numero)
+            {
+                int n = numero;
+                int digito;
+                int digitoElevado;
+                int tamanhoN = n.ToString().Length;
+                int soma = 0;
+
+                while (n > 0)
+                {
+                    
+                    digito = n % 10;
+
+                    digitoElevado = Convert.ToInt32(Math.Pow(digito, tamanhoN));
+                    soma = soma + digitoElevado;
+                    n = n / 10;
+
+                }
+
+                if (soma == numero) { return true; }
+                else { return false; }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            na a = new na();
+            
+
+            for (int i = 0; i <= 10000; i++)
+            {
+                a.IsArmstrong(i);
+                if (a.IsArmstrong(i)) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
