e4a9d76613eb7a2be793f7eaaa685673a1c7b760
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2022-12-07 09:23:48 -0300

    Adiciona exercício 3 (incompleto)

---
+++ Ex4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    internal class Program
+    {
+
+        public class itens
+        {
+            List<string> list = new List<string>();
+            public void RemoveRepetidos()
+            {
+                List<string> listaSemRepeticao = new List<string>();
+                foreach (var item in list)
+                {
+                    if (list.Contains(item))
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        listaSemRepeticao.Add(item);
+                    }
+                }
+                
+                for (int i = 0; i < list.Count(); i++)
+                {
+                    Console.WriteLine(listaSemRepeticao[i]);
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                itens a = new itens();
+                a.list.Add("1");
+                a.list.Add("1");
+                a.list.Add("1");
+                a.list.Add("1");
+                a.list.Add("2");
+                a.list.Add("3");
+
+                a.RemoveRepetidos();
+
+            }
+        }
+    }
+}
